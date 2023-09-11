e126e38d0b427b8e72d58bb23fb7175436e7d808
testlucik <testluciktest@gmail.com>
2023-09-12 01:00:05 +0300

    added new variables

--- src/main/java/com/tekwillacademy/Main.java
+++ src/main/java/com/tekwillacademy/Main.java
@@ -2,6 +2,39 @@ package com.tekwillacademy;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Declare boolean variables:
+
+        boolean isSuccessful;
+        boolean isDomestic;
+
+        //Declare chars:
+        char priority;
+
+        // Declare numbers:
+
+        byte currencyId;
+        short insitutionId;
+        int userID;
+        long paymentId;
+        float fees;
+        double amount;
+
+        // Initiate a payment:
+
+        isSuccessful = true;
+        isDomestic = true;
+        priority = 'A';
+        currencyId = 1;
+        insitutionId = 10;
+        userID = 102;
+        paymentId = 001115566L;
+        fees = 3.24F;
+        amount = 948.25;
+
+        System.out.println("The initiated payment has " + paymentId + " id and " + amount + " amount.");
+
+
     }
+
 }
